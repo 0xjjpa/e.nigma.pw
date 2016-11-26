698d18eb20e3c1a2c337cbcfc8fbceb536ba51b8
Jose Aguinaga <jj.perez.aguinaga@gmail.com>
2016-11-26 01:55:45 +0100

    Added sample fixture for testing actual sign verification through kbpgp keymanager fingerprint comparison against a user public key and a signed message

--- src/App.js
+++ src/App.js
@@ -1,6 +1,12 @@
 import React, { Component } from 'react'
+import { load } from './Encrypt'
+import { signedMessage } from './Fixtures'
 
 class App extends Component {
+  async componentWillMount () {
+    const isLegit = await load('jjperezaguinaga', signedMessage)
+    console.log('Are we legit?', isLegit)
+  }
   render () {
     return (
       <div className='App' />
---
+++ src/Fixtures.js
@@ -0,0 +1,17 @@
+export const signedMessage = `-----BEGIN PGP SIGNED MESSAGE-----
+Hash: SHA512
+
+This is a sample message signed by @jjperezaguinaga
+-----BEGIN PGP SIGNATURE-----
+Version: Keybase OpenPGP v2.0.58
+Comment: https://keybase.io/crypto
+
+wsBcBAABCgAGBQJYOMpcAAoJEEcNV2F2U20cE4gH/jhCSVZ3LUh6fnn9eZjkQi1t
++CKAZLWbHlMSZgIsyo1oy7oWxFk7sWOwN9D0SKa13R9ZU8ANGCPPFNKBew0GqYAQ
+CUUcFXAGmz+BIZ3UEUwby6KfK1oQs8wQabOerLYiO99YnABA68vWS7htj3VkdQyZ
+8op6GZCmtQCkoV47u7VsVoLOYGi50c3jsGfrrtcmBFlwd8GcA/iqMBxmZMCuw1MP
+C87d5sRwyHtcSScLjvMUO4zZKpVyI5BCa456YATroG5Z6AQP6/ItQaXpdPeer72i
+bGupiCtyoDHSSBz1AjSvkpvasD/wPBqQ4H9xTiNomSMH1g3x7EO8wCXc+zEwVQA=
+=0j+z
+-----END PGP SIGNATURE-----
+`
